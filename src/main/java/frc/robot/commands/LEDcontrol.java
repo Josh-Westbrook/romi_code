69d98a90e91a0ea208166e65fd4f4eb917f4cc1a
Josh Westbrook <joshwestbrook2008@gmail.com>
2023-09-27 19:21:07 -0500

    first commit

---
+++ src/main/java/frc/robot/commands/LEDcontrol.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.OnBoardIO;
+
+public class LEDcontrol extends CommandBase {
+  /** Creates a new LEDcontrol. */
+  private final OnBoardIO m_io;
+
+  public LEDcontrol(OnBoardIO io) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_io = io;
+    addRequirements(io);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_io.setGreenLed(false);
+  }
+  public void executeOn() {
+    m_io.setGreenLed(true);
+  }
+
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
